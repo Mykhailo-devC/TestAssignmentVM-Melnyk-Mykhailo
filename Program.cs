22bb014281ba79ce57b98557da686049e18bb76e
Mykhailo Melnyk <monstercattop@gmail.com>
2023-10-17 22:46:28 +0300

    Db Connection

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
-﻿using TestAssignmentVM_Melnyk_Mykhailo;
+﻿using System.Data.SqlClient;
+using TestAssignmentVM_Melnyk_Mykhailo;
 
 IConfigurationReader reader = new ConfigurationReader();
 
 Console.WriteLine("Connection string: {0}", reader.GetConnectionString());
+
+using(SqlConnection connection = new SqlConnection(reader.GetConnectionString()))
+{
+    connection.Open();
+    Console.WriteLine(connection.State);
+    connection.Close();
+}
+
+Console.WriteLine();
 Console.WriteLine("Parameters: ");
 reader.GetParameters().PrintAllPairs();
 
